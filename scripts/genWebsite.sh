b9f4d23c7aaa82407a05a555eb13a2ea6df090e0
Davide <tdavide@posteo.net>
2025-11-11 09:24:50 +0100

    fix: aggiornato script website e script compilatore

--- scripts/genWebsite.sh
+++ scripts/genWebsite.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 #Script utilizzato per generare le liste di verbali nel sito
+months=(Gennaio Febbraio Marzo Aprile Maggio Giugno Luglio Agosto Settembre Ottobre Novembre Dicembre)
 
 websiteUrl="https://grouprubberduck.github.io/Documentazione/"
 
 # Usage: replaceInFile "<pdf_path>" "<delimiter_name>""
 replaceInFile() {
     out="<!--%BEGIN_$2%-->"
-    for f in $(ls -1 "$1"*.pdf); do
+    for f in $(ls -1r "$1"*.pdf); do
         # Estrae la data dal verbale dal nome del PDFpdf. Il PDF deve essere nominato 'Nome_Completo_YYYY_MM_DD.pdf'.
         name="$(echo $f | rev | cut -d '/' -f 1 | rev)"
+        version="$(echo $name | cut -d '-' -f 4)"
+        version="${version::-4}"
+        date="$(echo $name | cut -d '_' -f 1)"
+        name="$(echo $name | cut -d '_' -f 2- | cut -d '-' -f 1)"
         # Aggiunge un elemento alla lista nel buffer
-        out="$out        <li><a href="$websiteUrl$f" target="_blank">${name::-4}</a></li>\n"
+        out="$out        <li><a href=\"$websiteUrl$f\" target=\"_blank\">$date</a> - <span class=\"file-version\">$version</span></li>\n"
     done
     out="$out<!--%END_$2%-->"
     # Rimpiazza i due delimitatori e il loro contenuto con il buffer creato
@@ -18,7 +23,7 @@ replaceInFile() {
 }
 
 # Candidatura
-replaceInFile "output/Candidatura/" "CANDIDATURA_VERBALI"
+# replaceInFile "output/Candidatura/" "CANDIDATURA_VERBALI"
 replaceInFile "output/Candidatura/DocumentazioneInterna/" "CANDIDATURA_VERBALI_INTERNI"
 replaceInFile "output/Candidatura/DocumentazioneEsterna/" "CANDIDATURA_VERBALI_ESTERNI"
 
