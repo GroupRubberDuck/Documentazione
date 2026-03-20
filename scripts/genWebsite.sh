c2d0c1de30bbbf0a2a37ca3b375dc66984bfaa04
Davide <tdavide@posteo.net>
2026-03-20 14:26:26 +0100

    fix: script per la generazione del sito + aggiunti link mancanti al sito

--- scripts/genWebsite.sh
+++ scripts/genWebsite.sh
@@ -4,18 +4,22 @@ months=(Gennaio Febbraio Marzo Aprile Maggio Giugno Luglio Agosto Settembre Otto
 
 websiteUrl="https://grouprubberduck.github.io/Documentazione/"
 
-# Usage: replaceInFile "<pdf_path>" "<delimiter_name>""
+# Usage: replaceInFile "<pdf_path>" "<delimiter_name>"
 replaceInFile() {
     out="<!--%BEGIN_$2%-->"
     for f in $(ls -1r "$1"*.pdf); do
-        # Estrae la data dal verbale dal nome del PDFpdf. Il PDF deve essere nominato 'Nome_Completo_YYYY_MM_DD.pdf'.
         name="$(echo $f | rev | cut -d '/' -f 1 | rev)"
-        version="$(echo $name | cut -d '-' -f 4)"
-        version="${version::-4}"
-        date="$(echo $name | cut -d '_' -f 1)"
-        name="$(echo $name | cut -d '_' -f 2- | cut -d '-' -f 1)"
+        version="$(echo $name | grep -oE 'v[0-9]+\.[0-9]+\.[0-9]+')"
+        title="$(echo $name | cut -d '_' -f 1)"
+        type="$(echo $name | cut -d '_' -f 2- | cut -d '-' -f 1 | cut -d '_' -f 1)"
+        if [ ! "$type" = "Verbale" ] && [ ! "$type" = "Diario" ]; then
+            title="$(echo $name | cut -d '-' -f 1 | tr '_' ' ')"
+        fi
+        if [ "$type" = "Diario" ] && [ "$version" = "" ]; then
+            version="v1.0"
+        fi
         # Aggiunge un elemento alla lista nel buffer
-        out="$out        <li><a href=\"$websiteUrl$f\" target=\"_blank\">$date</a> - <span class=\"file-version\">$version</span></li>\n"
+        out="$out        <li><a href=\"$websiteUrl$f\" target=\"_blank\">$title</a> - <span class=\"file-version\">$version</span></li>\n"
     done
     out="$out<!--%END_$2%-->"
     # Rimpiazza i due delimitatori e il loro contenuto con il buffer creato
@@ -25,16 +29,21 @@ replaceInFile() {
 # RTB
 replaceInFile "output/RTB/DocumentazioneEsterna/VerbaliEsterni/" "RTB_VERBALI_ESTERNI"
 replaceInFile "output/RTB/DocumentazioneInterna/VerbaliInterni/" "RTB_VERBALI_INTERNI"
+replaceInFile "output/RTB/DocumentazioneEsterna/Analisi_dei_Requisiti/" "RTB_ADR"
+replaceInFile "output/RTB/DocumentazioneEsterna/Piano_di_Progetto/" "RTB_PDP"
+replaceInFile "output/RTB/DocumentazioneEsterna/Piano_di_qualifica/" "RTB_PDQ"
+replaceInFile "output/RTB/DocumentazioneInterna/Norme_progetto/" "RTB_NDP"
 
 # Candidatura
-# replaceInFile "output/Candidatura/" "CANDIDATURA_VERBALI"
+replaceInFile "output/Candidatura/" "CANDIDATURA_VERBALI"
 replaceInFile "output/Candidatura/DocumentazioneInterna/" "CANDIDATURA_VERBALI_INTERNI"
 replaceInFile "output/Candidatura/DocumentazioneEsterna/" "CANDIDATURA_VERBALI_ESTERNI"
 
 # Diario di bordo
 replaceInFile "output/DiariDiBordo/" "DIARIO_DI_BORDO"
 
-BASE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
-SRC_DIR="$BASE_DIR/src"
-path_dizionario="$SRC_DIR/RTB/DocumentazioneInterna/dizionario.typ"
-typst query --root "$BASE_DIR" --format json --field value --one "$path_dizionario" "<dizionario>" | python3 ./scripts/genGlossario.py > glossario.html 
+# Disattivato dato che ora il glossario viene gestito in un altro modo
+# BASE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
+# SRC_DIR="$BASE_DIR/src"
+# path_dizionario="$SRC_DIR/RTB/DocumentazioneInterna/dizionario.typ"
+# typst query --root "$BASE_DIR" --format json --field value --one "$path_dizionario" "<dizionario>" | python3 ./scripts/genGlossario.py > glossario.html 
